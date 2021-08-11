47f43f5d85f82ff135ff59ea541eba98fb311c93
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-11 17:40:51 +0000

    Finished the character counter and it works. Currently working on the html and css behind the actual tweet. Created the layout with the html code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,16 @@
 //Helps test to see if jquery is working so you don't manipulate the DOM by accident
-$(document).ready(function() {
-  console.log("testing testing")
-})
-
-$(".new-tweet #tweet-text").on("input", function(event) {
-  const charCount = 140 - $(this).val().length;
-  if (charCount < 0) {
-    $(this).
+$(document).ready(function(event) {
+$("#tweet-text").on("input", function(event) {
+  const maxChars = 140;
+  const charCount = maxChars - $(this).val().length;
+  const counter = $(this).siblings("#submit-tweet").children(".counter");
+  console.log(charCount);
+  counter.html(charCount);
+  if (charCount > 0) {
+    counter.css("color", "black");
+  } else {
+    counter.css("color", "red");
   }
+
+  });
 });
