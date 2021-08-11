671acba665ef73f4b3bad3085b717088a80b445d
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-11 13:11:46 +0000

    used jquery to find the length of the text inputted in the text area and tested using the browser console. Currently working on a way to change the counter as the number of characters changes.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,11 @@
 //Helps test to see if jquery is working so you don't manipulate the DOM by accident
-// $(document).ready(function() {
-//   console.log("testing testing")
-// })
+$(document).ready(function() {
+  console.log("testing testing")
+})
 
-$(".new-tweet textarea").on("input", function(event) {
-  console.log(this.event);
+$(".new-tweet #tweet-text").on("input", function(event) {
+  const charCount = 140 - $(this).val().length;
+  if (charCount < 0) {
+    $(this).
+  }
 });
