5081998766ebefca6b6b162b72df149958659adb
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-12 18:38:05 +0000

    Added conditions in the submit event listener that restricts the user from exceeding the character limit or posting an empty tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -52,33 +52,52 @@
               `
               $('#tweetContainer').append(tweetContent);
           };
-      
-      $('#tweet-form').submit(function(event) {
-        event.preventDefault();
-        
-        $.ajax({
-          type: "POST",
-          url: '/tweets',
-          data: $(this).serialize(),
-          success: function(data) {
-            renderTweets(data)
-          }
-        });
-        
-      });
+          
+          //Declaring a counter for the input characters and counting the characters 
+          //in the input event listener so that it can be referenced in the submit request
 
-      function loadTweets() {
-        $.ajax({
-          type: "GET",
-          url: '/tweets',
-          success: function(data) {
-            renderTweets(data)
-          }
+          let charCountVal;
+          
+          $("#tweet-text").on("input", function(event) {
+            charCountVal = $(this).val().length;
         });
 
-      }
+        $('#tweet-form').submit(function(event) {
+          event.preventDefault();
+         
+          //Form Validation
 
-      loadTweets();
+          if (charCountVal > 140) {
+            alert("Sorry, you've exceeded the character limit of 140 characters! :(")
+          } else if (charCountVal === 0) {
+            alert("Sorry, you're tweet must contain at least 1 character! :(")
+          } else {
+          
+            $.ajax({
+              type: "POST",
+              url: '/tweets',
+              data: $(this).serialize(),
+              success: function(data) {
+                renderTweets(data)
+              }
+            });
+
+          }
+          
+        });
+
+        function loadTweets() {
+          $.ajax({
+            type: "GET",
+            url: '/tweets',
+            success: function(data) {
+              renderTweets(data)
+            }
+          });
+
+        }
+
+        loadTweets();
 
       });
 
