078cacebfc7789fc03e0491a24759fd8358de7bd
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-14 12:39:11 +0000

    Added a new container around the icon and name of the user so that they are grouped together. Also, adjusted the styling so that the users handle is properly placed in the top right corner of the tweet and doesn't go outside of the container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,44 +3,44 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-  
-  
-  $(document).ready(function(event) {
-     
-      const renderTweets = function(tweets) {
-        // loops through tweets
-        $('#tweetContainer').empty();
-        for (tweet of tweets) {
-          $('#tweetContainer').prepend(createTweet(tweet));
-        };
-      
-        };
 
-        //This function helps avoid Cross-Site Scripting
-        const escape = function (str) {
-          let div = document.createElement("div");
-          div.appendChild(document.createTextNode(str));
-          return div.innerHTML;
-        };
+$(document).ready(function (event) {
+  const renderTweets = function (tweets) {
+    // loops through tweets
+    $("#tweetContainer").empty();
+    for (tweet of tweets) {
+      $("#tweetContainer").prepend(createTweet(tweet));
+    }
+  };
 
+  //This function helps avoid Cross-Site Scripting
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-      
-      const createTweet = function(tweet) {
-        // console.log(tweet);
-        const time = timeago.format(1461116232227)
-            const tweetContent = `
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+  const createTweet = function (tweet) {
+    // console.log(tweet);
+    const time = timeago.format(tweet.created_at);
+    const tweetContent = `
             <section class = "tweet-container">
                 <article class = "tweet">
-                  <header >
+                  <header>
+                  <span>
                     <img src=${escape(tweet.user.avatars)} alt="Avatar"> 
                     <h2 class = "name">${escape(tweet.user.name)}</h3>
+                    </span>
                     <h2 class = "username">${escape(tweet.user.handle)}</h3>
                   </header>
+                  <div class = "tweet-body">
                   <p>
                    ${escape(tweet.content.text)}
                   </p>
+                  </div>
                  
                   <footer>
                       <date>
@@ -56,70 +56,72 @@
                 
                 </article>
               </section>
-              `
-              $('#tweetContainer').prepend(tweetContent);
-          };
+              `;
+    $("#tweetContainer").prepend(tweetContent);
+  };
 
-         
-          
-          //Declaring a counter for the input characters and counting the characters 
-          //in the input event listener so that it can be referenced in the submit request
+  //Declaring a counter for the input characters and counting the characters
+  //in the input event listener so that it can be referenced in the submit request
 
-          let charCountVal = 0;
-          
-          $("#tweet-text").on("input", function(event) {
-            charCountVal = $(this).val().length;
-        });
+  let charCountVal = 0;
 
-        $('#tweet-form').submit(function(event) {
-          event.preventDefault();
-         
-          //Form Validation
+  $("#tweet-text").on("input", function (event) {
+    charCountVal = $(this).val().length;
+  });
 
-          if (charCountVal > 140) {
-             //Removing error message as soon as the input is submitted
-            $("div.error-message").remove();
-            $(".error-message").removeClass("error-message");
+  $("#tweet-form").submit(function (event) {
+    event.preventDefault();
 
-            $('.new-tweet').prepend($(`<div class = error-message>Sorry, you've exceeded the character limit of 140 characters! :( </div>`)).slideDown(700);
-          } else if (charCountVal === 0) {
-             //Removing error message as soon as the input is submitted
-            $("div.error-message").remove();
-            $(".error-message").removeClass("error-message");
+    //Form Validation
 
-            $('.new-tweet').prepend($(`<div class = error-message>Sorry, you're tweet must contain at least 1 character! :( </div>`)).slideDown(700);
-          } else {
-            //Removing error message as soon as the input is submitted
-            $("div.error-message").remove();
-            $(".error-message").removeClass("error-message");
-          $.ajax({
-            type: "POST",
-            url: '/tweets',
-            data: $(this).serialize(),
-            success: function(data) {
-              loadTweets();
-              history.go(0);
-            }
-          });
-        }
+    if (charCountVal > 140) {
+      //Removing error message as soon as the input is submitted
+      $("div.error-message").remove();
+      $(".error-message").removeClass("error-message");
 
-          
-          
-        });
+      $(".new-tweet")
+        .prepend(
+          $(
+            `<div class = error-message>Sorry, you've exceeded the character limit of 140 characters! :( </div>`
+          )
+        )
+        .slideDown(700);
+    } else if (charCountVal === 0) {
+      //Removing error message as soon as the input is submitted
+      $("div.error-message").remove();
+      $(".error-message").removeClass("error-message");
 
-        function loadTweets() {
-          $.ajax({
-            type: "GET",
-            url: '/tweets',
-            success: function(data) {
-              renderTweets(data)
-            }
-          });
-        }
-        loadTweets();
-        
+      $(".new-tweet")
+        .prepend(
+          $(
+            `<div class = error-message>Sorry, you're tweet must contain at least 1 character! :( </div>`
+          )
+        )
+        .slideDown(700);
+    } else {
+      //Removing error message as soon as the input is submitted
+      $("div.error-message").remove();
+      $(".error-message").removeClass("error-message");
+      $.ajax({
+        type: "POST",
+        url: "/tweets",
+        data: $(this).serialize(),
+        success: function (data) {
+          loadTweets();
+          history.go(0);
+        },
       });
+    }
+  });
 
-
-
-
+  function loadTweets() {
+    $.ajax({
+      type: "GET",
+      url: "/tweets",
+      success: function (data) {
+        renderTweets(data);
+      },
+    });
+  }
+  loadTweets();
+});
