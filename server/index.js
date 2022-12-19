2c84c588c8c99b08b8c571979216a627a1c6023e
Yusuf <y.abdulla37@gmail.com>
2022-12-18 23:05:01 -0700

    takes port from env

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 // Basic express setup:
 
-const PORT          = 8080;
+const PORT          = process.env.PORT;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
